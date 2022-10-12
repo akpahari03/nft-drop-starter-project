7c961eafc5d2a276ddac2dc3f1a5e52f64395234
akpahari03 <ak.pahari03@gmail.com>
2022-10-12 23:07:12 +0530

    message

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -4,6 +4,8 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { useWallet } from "@solana/wallet-adapter-react";
+
 
 import "../styles/App.css";
 import "../styles/globals.css";
@@ -14,6 +16,8 @@ const App = ({ Component, pageProps }) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
     const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
+    // const wallet = useWallet();
+
 
     return (
         <ConnectionProvider endpoint={endpoint}>
@@ -23,6 +27,38 @@ const App = ({ Component, pageProps }) => {
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
+    );
+};
+
+const Home = () => {
+    const wallet = useWallet();
+    // Actions
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+            <div className="button-container">
+                <WalletMultiButton className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <p className="header">🍭 Candy Drop</p>
+                    <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {wallet.publicKey ? "Hello" : renderNotConnectedContainer()}
+                </div>
+
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
+        </div>
     );
 };
 
